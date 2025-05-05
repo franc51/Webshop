12aae7d5b268b86db53547bbebb84d18b1fcf0fe
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-05-05 13:18:07 +0300

    ng testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/Login/login/login.component';
+import { SignupComponent } from './components/Signup/signup/signup.component';
+import { HomeComponent } from './components/Home/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "login", component: LoginComponent},
+  {path: "signup", component: SignupComponent},
+  {path: "home", component: HomeComponent},
+];
