6edfcd0d1c4c21f69c5af15c91750fa3473e9aa2
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-05-21 13:09:12 +0300

    added firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,19 @@ import {
 } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { getAnalytics, Analytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0OIwQCMH2lJ0SMirNj7bNghzMKrjRf9E",
+  authDomain: "cyber-aed44.firebaseapp.com",
+  projectId: "cyber-aed44",
+  storageBucket: "cyber-aed44.firebasestorage.app",
+  messagingSenderId: "535759361456",
+  appId: "1:535759361456:web:f2fcf0901b00e5b8b07275",
+  measurementId: "G-R7F3HRY9MT"
+};
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -18,10 +31,16 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+
   title = 'Webshop';
   loading = false;
 
+  private app: FirebaseApp;
+  private analytics: Analytics;
+
   constructor(private router: Router) {
+    this.app = initializeApp(firebaseConfig);
+    this.analytics = getAnalytics(this.app);
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
         this.loading = true;
