b881d89e33bd141b144d7746f7e04c1b4080f016
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-05-23 13:27:51 +0300

    small fixes

--- src/app/components/Home/home/home.component.ts
+++ src/app/components/Home/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductCategoryComponent } from '../../Product-category/product-category/product-category.component';
-import { ProductListComponent } from '../../Product-list/product-list/product-list.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, ProductCategoryComponent, ProductListComponent],
+  imports: [RouterModule, ProductCategoryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
